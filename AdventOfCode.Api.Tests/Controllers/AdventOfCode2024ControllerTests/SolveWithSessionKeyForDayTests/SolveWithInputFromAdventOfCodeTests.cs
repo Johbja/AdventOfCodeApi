ef0bd823e2732f1fab35e75e94befe2b9a60b687
Johannes Bjärehall <jojeskate@hotmail.com>
2025-06-03 00:57:55 +0200

    Adds project restructure Adds initial tests

---
+++ AdventOfCode.Api.Tests/Controllers/AdventOfCode2024ControllerTests/SolveWithSessionKeyForDayTests/SolveWithInputFromAdventOfCodeTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Controllers.AdventOfCode2024;
+
+public class SolveWithInputFromAdventOfCodeTests
+{
+    [Fact]
+    public void Test1()
+    {
+
+        //if (payload.ContentType != "text/plain")
+        //    throw new ArgumentException("payload is not a text file");
+
+        //using var reader = new StreamReader(payload.OpenReadStream());
+        //var fileContent = await reader.ReadToEndAsync();
+
+        //var input = new AdventOfCode2024DaySelectionOperation.Input(day, fileContent);
+        //var traceableOutput = CreateOperation<AdventOfCode2024DaySelectionOperation>().Execute(input);
+        //var result = await CreateBaseResponse(traceableOutput);
+        //return Ok(result);
+
+        Assert.Equal(1, 1); // Placeholder assertion for the test to compile and run
+
+    }
+}
